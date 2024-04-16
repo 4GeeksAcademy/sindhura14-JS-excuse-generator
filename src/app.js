b67f4884fe05aaeaaf370e204bc1d8b5efdeb88a
sindhura14 <sindhu.kilaru14@gmail.com>
2024-04-16 00:23:30 +0000

    code for excuse generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "crushed", "broke", "peed"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class.",
+    "when I was sleeping.",
+    "while I was exercising.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  let excuseGenerated =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
+
+  document.getElementById("excuse").innerHTML = excuseGenerated;
 };
